8f46465072f21785ae3ddf692f8b0f65f21b85ee
wuchuheng <root@wuchuheng.com>
2022-08-01 12:49:42 +0800

    feat(view): initialized the editor.

--- lib/pages/home_page/devices/lg1024/edit_section/index.dart
+++ lib/pages/home_page/devices/lg1024/edit_section/index.dart
@@ -12,10 +12,18 @@ class _EditSectionState extends State<EditSection> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: MediaQuery.of(context).size.height - CommonConfig.toolBarHeight,
-        color: Colors.blue[100],
-        width: MediaQuery.of(context).size.width -
-            CommonConfig.lg1024DirectoryWidth -
-            CommonConfig.lg1024ListWidth);
+      height: MediaQuery.of(context).size.height - CommonConfig.toolBarHeight,
+      color: Colors.blue[100],
+      width: MediaQuery.of(context).size.width -
+          CommonConfig.lg1024DirectoryWidth -
+          CommonConfig.lg1024ListWidth,
+      child: const TextField(
+        maxLines: 200,
+        decoration: InputDecoration.collapsed(
+          border: InputBorder.none,
+          hintText: "",
+        ),
+      ),
+    );
   }
 }
