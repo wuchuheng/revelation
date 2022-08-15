6c4813921f2509c8de6f54889e1bafce56c8bf30
wuchuheng <root@wuchuheng.com>
2022-08-16 07:03:55 +0800

    refactor: simplify the code in direcory.

--- lib/pages/home_page/devices/lg1024/directory_section/create_button.dart
+++ lib/pages/home_page/devices/lg1024/directory_section/create_button.dart
@@ -3,88 +3,27 @@ import 'package:snotes/common/iconfont.dart';
 import 'package:snotes/config/config.dart';
 import 'package:snotes/service/directory_service/index.dart';
 
-class MenuItemData {
-  final String name;
-  final String value;
-
-  MenuItemData({required this.name, required this.value});
-}
-
 class CreateButton extends StatelessWidget {
   final double bottomBarHeight;
-  static String createFolderValue = 'Folder';
 
-  List<MenuItemData> menuList = [
-    MenuItemData(name: 'Folder', value: createFolderValue),
-    MenuItemData(name: 'Smart Folder', value: 'Smart Folder'),
-  ];
-
-  CreateButton({Key? key, required this.bottomBarHeight}) : super(key: key);
-
-  void handleSelect(String value, BuildContext context) async {
-    if (value == createFolderValue) {
-      await DirectoryService.create();
-      return;
-    }
-  }
-
-  List<PopupMenuItem<String>> getPopupMenuItem(BuildContext context) {
-    const double fontSize = 17.0;
-    return menuList
-        .map((e) => PopupMenuItem(
-              value: e.value,
-              child: Row(
-                children: [
-                  const Icon(IconFont.icon_setting, size: fontSize),
-                  const SizedBox(width: 5),
-                  Text(e.name, style: const TextStyle(fontSize: fontSize))
-                ],
-              ),
-            ))
-        .toList();
-  }
-
-  void testImap() async {}
-
-  Widget getLabel() {
-    return Row(
-      children: [
-        Icon(IconFont.icon_plus, size: 13, color: Config.textGrey),
-        Text(
-          ' New Folder',
-          style: TextStyle(color: Config.textGrey),
-        ),
-      ],
-    );
-  }
-
-  Widget getConTainer({required Widget child}) {
-    return SizedBox(
-      height: bottomBarHeight,
-      width: double.infinity,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [child],
-      ),
-    );
-  }
+  const CreateButton({Key? key, required this.bottomBarHeight}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return getConTainer(
-      child: PopupMenuButton<String>(
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(15.0),
-          ),
+    return InkWell(
+      onTap: DirectoryService.create,
+      child: SizedBox(
+        height: bottomBarHeight,
+        width: double.infinity,
+        child: Row(
+          children: [
+            Icon(IconFont.icon_plus, size: 13, color: Config.textGrey),
+            Text(
+              ' New Folder',
+              style: TextStyle(color: Config.textGrey),
+            ),
+          ],
         ),
-        itemBuilder: getPopupMenuItem,
-        offset: const Offset(80, 100),
-        elevation: 2,
-        // on selected we show the dialog box
-        onSelected: (value) => handleSelect(value, context),
-        child: getLabel(),
       ),
     );
   }
