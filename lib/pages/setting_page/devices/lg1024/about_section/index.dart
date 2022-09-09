af6dd4a20cc4636c91f4f2a441ba47d2da718f11
wuchuheng <root@wuchuheng.com>
2022-09-09 14:07:29 +0800

    feat(view): init about tab in setting page.

---
+++ lib/pages/setting_page/devices/lg1024/about_section/index.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:snotes/config/config.dart';
+
+class AboutSection extends StatelessWidget {
+  const AboutSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const String assetName = 'assets/images/svg/undraw_no_data_re_kwbl.svg';
+    final Widget svg = SvgPicture.asset(
+      assetName,
+      height: 110,
+    );
+    Widget container(Widget child) => Container(margin: const EdgeInsets.only(top: 10), child: child);
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        svg,
+        container(Text(
+          Config.appName,
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+        )),
+        container(const Text('Version 1.0.1')),
+        container(
+          Text('Copyright © 2022 com.wuchuheng.snotes. All rights reserved.',
+              style: TextStyle(color: Colors.grey[400]!)),
+        ),
+      ],
+    );
+  }
+}
--- lib/pages/setting_page/devices/lg1024/index.dart
+++ lib/pages/setting_page/devices/lg1024/index.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:snotes/pages/setting_page/devices/lg1024/about_section/index.dart';
 import 'package:snotes/pages/setting_page/devices/lg1024/log_section/index.dart';
 import 'package:snotes/pages/setting_page/devices/lg1024/tab_section/header_section.dart';
 import 'package:snotes/pages/setting_page/devices/lg1024/user_section/index.dart';
@@ -17,9 +18,9 @@ class _LG1024SettingPageState extends State<LG1024SettingPage> {
     TabItem(icon: Icons.settings, text: 'General', body: const Text('General')),
     TabItem(icon: Icons.people, text: 'User', body: const UserSection()),
     TabItem(icon: Icons.notes, text: 'Logs', body: const LogSection()),
-    TabItem(icon: Icons.info_outline, text: 'About', body: const Text('About')),
+    TabItem(icon: Icons.info_outline, text: 'About', body: const AboutSection()),
   ];
-  int activeIndex = 3;
+  int activeIndex = 0;
 
   @override
   Widget build(BuildContext context) {
